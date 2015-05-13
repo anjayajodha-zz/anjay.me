8eeeb396dd1723c441d84a97a86343375f3dccfa
Anjay Ajodha <anjay@anjay.me>
2015-05-13 13:28:49 -0500

    Fixed div

--- resume-test.php
+++ resume-test.php
@@ -91,7 +91,8 @@
 		  			<div class='text-center'>
 			    		<h2 class="title"><span>Education</span></h2>
 			    	</div>
-					<div class="col-md-6 wow slideInLeft">
+				</div>
+				<div class="col-md-6 wow slideInLeft">
 					<h4>University of Houston:</h4>
 					<ul class="circle">
 						<li>Bachelor of Science in Computer Science
@@ -104,7 +105,6 @@
 				</div>
 				<div class="col-md-6 wow">
 	  			</div>
-	  			</div>
 			</div><!--/ Skills row end -->
 			<div class="gap-40"></div>
 			<div class="row">
@@ -112,6 +112,19 @@
 		  			<div class='text-center'>
 			    		<h2 class="title"><span>Honors & Awards</span></h2>
 			    	</div>
+	  			</div>
+				  <div class="col-md-6 wow slideInLeft">
+					<h4>University of Houston:</h4>
+					<ul class="circle">
+						<li>Bachelor of Science in Computer Science
+						</li>
+						<li>Minors in Leadership Studies and Mathematics
+						</li>
+						<li>Expected Graduation Date: May 2015
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-6 wow">
 	  			</div>
 			</div><!--/ Skills row end -->
 			<div class="gap-40"></div>
